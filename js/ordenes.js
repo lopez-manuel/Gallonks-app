fd8a763da42149d618ca2457106d52331bdd6937
lopez-manuel <lopez.ledezma.manuel@gmail.com>
2022-10-31 12:50:56 -0600

    caracteristica de previsualizacion terminada

--- js/ordenes.js
+++ js/ordenes.js
@@ -3,58 +3,106 @@ const product = document.getElementById("product")
 const portion = document.getElementById("portion")
 const sauce = document.getElementById("sauce")
 const addProductButton = document.getElementById("add-product-button")
+const finishOrderButton = document.getElementById("end-order")
 const previewOrder = document.getElementById("preview")
 let previewClientName = document.getElementById("preview-order-name")
 let previewOrderProducts = document.getElementById("preview-order-products")
 let previewOrderCost = document.getElementById("preview-order-cost")
+const extras = document.getElementById("extras")
 let productsInOrder = 0
 let productsNameInOrder = []
-
-portion.style.display = "none"
-sauce.style.display = "none"
+let today = new Date()
 
 
-function deletes(mensaje){
-    alert("se elimino el boton " + mensaje)
-}
+getDate()
+
+
 
 clientName.addEventListener("keydown" && "keyup", namePreview )
 product.addEventListener("change",cambio)
 addProductButton.addEventListener("click", addProduct)
+finishOrderButton.addEventListener("click", finishOrder)
 
+
+
+//NAME PREVIEW 
 function namePreview(){
     previewClientName.innerHTML = `<p>Nombre</p>
     <p>${clientName.value}</p>`
 }
 
+
+//PRODUCT SELECTION
 function cambio(){
     if(product.value === "extras"){
-        alert("entro a extras")
-        portion.style.display = "none"
-        sauce.style.display = "none"
+        extras.style.display = "inline"
+        document.querySelector(".portion").style.display = "none"
+        document.querySelector(".sauces").style.display = "none"
+        document.querySelector(".extras").style.display = "block"
+    }
+
+    else if(product.value === ""){
+        document.querySelector(".portion").style.display = "none"
+        document.querySelector(".sauces").style.display = "none"
+        document.querySelector(".extras").style.display = "none"
     }
 
     else{
-        portion.style.display = "inline"
-        sauce.style.display = "inline"
+        document.querySelector(".portion").style.display = "block"
+        document.querySelector(".sauces").style.display = "block"
+        document.querySelector(".extras").style.display = "none"
     }
 }
 
 
+//ADD NEW PRODUCT
 function addProduct (){
-    productsInOrder++
-    productsNameInOrder[productsInOrder] = portion.value + " de " + product.value + " " + sauce.value
-    console.log(productsNameInOrder)
-    previewOrderProducts.innerHTML += `<p> ${productsNameInOrder[productsInOrder]} <button onclick="deleteProduct(${productsInOrder})">x</button> </p>`
+    
+    if(product.value === "extras"){
+        if(extras.value === ""){
+            alert("Selecciona un producto valido")
+        }
 
-    portion.value = ""
-    portion.style.display = "none"
-    sauce.value = ""
-    sauce.style.display = "none"
-    product.value = ""
-    orderCost()
+        else{
+            productsInOrder++
+            console.log(extras.value + " <---- esta fue la opcion")
+            productsNameInOrder[productsInOrder] = extras.value
+            previewOrderProducts.innerHTML += `<p> ${productsNameInOrder[productsInOrder]} <button onclick="deleteProduct(${productsInOrder})">x</button> </p>`
+            product.value = ""
+            extras.value = ""
+            document.querySelector(".extras").style.display = "none"
+            orderCost()
+        }
+        
+
+
+
+    }
+
+    else{
+        if(portion.value === "" | product.value === "" | sauce.value === ""){
+            alert("hay algo mal en tu orden")
+        }
+
+        else{
+            productsInOrder++
+        productsNameInOrder[productsInOrder] = portion.value + " de " + product.value + " " + sauce.value
+        console.log(productsNameInOrder)
+        previewOrderProducts.innerHTML += `<p> ${productsNameInOrder[productsInOrder]} <button onclick="deleteProduct(${productsInOrder})">x</button> </p>`
+
+        portion.value = ""
+        sauce.value = ""
+        product.value = ""
+        document.querySelector(".portion").style.display = "none"
+        document.querySelector(".sauces").style.display = "none"
+        orderCost()
+        }
+    }
 }
 
+
+
+//DELETE PRODUCT
 function deleteProduct(product){
     let x = 1
     productsNameInOrder.splice(product,1)
@@ -69,26 +117,70 @@ function deleteProduct(product){
         orderCost()
     });
 
+    productsInOrder = productsNameInOrder.length -1
+    console.log("hay " + productsInOrder + " productos en la orden")
     console.log(productsNameInOrder)
 }
 
+
+
+//ORDER TOTAL $
 function orderCost(){
     let mediosKilos = 0
     let kilos = 0
+    let totalCost = 0
     productsNameInOrder.forEach(product =>{
 
-        if(product.indexOf("1/2") > -1){
+        if(product.indexOf("1/2 kg de Alitas") > -1){
             mediosKilos++
-            console.log("hay " + mediosKilos + " medios jen la orden")
+            totalCost += 110
         }
 
-        else if (product.indexOf("1 kg") > -1){
+        else if (product.indexOf("1 kg de Alitas") > -1){
             kilos++
-            console.log("hay " + kilos + " kg en la orden")
+            totalCost += 200
         }
 
-        else {
-
+        else if (product.indexOf("1/2 kg de boneless") > -1) {
+            mediosKilos++
+            totalCost += 130
         }
+
+        else if (product.indexOf("1 kg de boneless") > -1){
+            totalCost += 250
+        }
+        else if (product.indexOf("papas a la francesa") > -1 | product.indexOf("aros de cebolla") > -1 ){
+            console.log("seleccionaste papas pero no aparece en el costo")
+            totalCost += 50
+        }
+        else {}
     })
+
+    previewOrderCost.innerHTML = `<p>Total</p>
+    <p>$${totalCost}</p>`
+
+    console.log("hay " + mediosKilos + " medio kilo en la orden")
+    console.log("hay " + kilos + " kg en la orden")
+    console.log("Tu orden es de: $" + totalCost)
+}
+
+//FINISH ORDER
+
+function finishOrder(){
+    if(productsNameInOrder.length < 2){
+        alert("no tienes productos en tu orden")
+    }
+
+    else{
+        alert("tienes "+(productsNameInOrder.length -1)+ " pedidos en tu orden")
+    }
+}
+
+//DATE
+function getDate(){
+    let todayF = today.getDate() + "/" + (today.getMonth() +1) + "/" + today.getFullYear()
+    let datePrint = document.querySelector(".date")
+    datePrint.innerHTML = todayF
+    console.log("entro a la funcion")
+    console.log(datePrint)
 }
